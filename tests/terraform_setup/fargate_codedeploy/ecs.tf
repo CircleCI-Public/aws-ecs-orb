1d22974691d4f2ef6dc1c0d434c7b592f0e49018
Stella Lok <oinvertedworld@gmail.com>
2019-10-07 16:51:40 +0800

    add execution role to task definition so that the ECR container image can be pulled

--- tests/terraform_setup/fargate_codedeploy/ecs.tf
+++ tests/terraform_setup/fargate_codedeploy/ecs.tf
@@ -8,6 +8,7 @@ resource "aws_ecs_task_definition" "ecs_task_dfn" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = 1024
   memory                   = 2048
+  execution_role_arn = "${aws_iam_role.ecs_task_execution_role.arn}"
 
   container_definitions = <<DEFINITION
 [
