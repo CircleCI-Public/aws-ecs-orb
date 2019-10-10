3e212859dc89fc413abae9bb24b8079867e4eb2c
Afraz Khan <khan.afraz4183@yahoo.com>
2019-10-11 02:22:45 +0500

    Update get_task_definition_value.py
    refactoring code for better understanding.

--- src/python/get_task_definition_value.py
+++ src/python/get_task_definition_value.py
@@ -4,22 +4,23 @@ import json
 
 
 def run(element_name, task_definition_str):
+    
     try:
         definition = json.loads(task_definition_str)
         task_definition = definition['taskDefinition']
     except:
         raise Exception('No valid task definition found: ' +
                         task_definition_str)
+        
     str_list_types = ['requiresCompatibilities']
     json_arr_types = ['placementConstraints', 'volumes', 'tags']
     json_obj_types = ['proxyConfiguration']
+    
     if element_name in json_arr_types:
         output_value = '[]'
     elif element_name in json_obj_types:
         output_value = '{}'
-    else:
-        output_value = ''
-    if element_name == 'tags':
+    elif element_name == 'tags':
         if element_name in definition:
             element_value = definition[element_name]
             output_value = json.dumps(element_value)
@@ -31,6 +32,9 @@ def run(element_name, task_definition_str):
             output_value = json.dumps(element_value)
         else:
             output_value = str(element_value)
+    else:
+        output_value=""
+        
     return output_value
 
 
