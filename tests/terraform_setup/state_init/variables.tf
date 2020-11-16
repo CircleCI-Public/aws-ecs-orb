a55dd056fab10140573039eec989b8292e18ad53
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-11-16 14:07:07 -0500

    re-add sample app tests

---
+++ tests/terraform_setup/state_init/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {
+  description = "AWS region e.g. us-east-1"
+}
---
+++ tests/test_app/src/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func mainHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World! (Version info: %s, build date: %s)", os.Getenv("VERSION_INFO"), os.Getenv("BUILD_DATE"))
+	})
+}
+
+func main() {
+	http.HandleFunc("/", mainHandler())
+	http.ListenAndServe(":8080", nil)
+}
