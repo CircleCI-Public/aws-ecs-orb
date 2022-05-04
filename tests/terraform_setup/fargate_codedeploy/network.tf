0aab2e7933957b558db168697e63483229a1acab
Brian Vu <emailbrian.vu@gmail.com>
2022-05-04 12:24:39 -0700

    fix: updated terraform version to latest

--- tests/terraform_setup/fargate_codedeploy/network.tf
+++ tests/terraform_setup/fargate_codedeploy/network.tf
@@ -27,9 +27,9 @@ resource "aws_subnet" "public" {
 
 # Internet Gateway for the public subnet
 resource "aws_internet_gateway" "gw" {
-  timeouts{
-    delete = "60m"
-  }
+  # timeouts{
+  #   delete = "60m"
+  # }
   vpc_id = aws_vpc.main.id
 }
 
--- tests/terraform_setup/fargate_codedeploy/terraform.tf
+++ tests/terraform_setup/fargate_codedeploy/terraform.tf
@@ -12,7 +12,7 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.aws_region
-  version    = "~> 2.7"
+  #version    = "~> 2.7"
 }
 
 locals {
