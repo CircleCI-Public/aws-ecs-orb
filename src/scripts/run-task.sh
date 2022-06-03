cdc117eb2286899c26bb7c9572885a71f9727169
Brian Vu <emailbrian.vu@gmail.com>
2022-06-02 19:12:23 -0700

    test: debugging

--- src/scripts/run-task.sh
+++ src/scripts/run-task.sh
@@ -19,7 +19,7 @@ if [ -n "$ECS_PARAM_GROUP" ]; then
 fi
 if [ -n "$ECS_PARAM_OVERRIDES" ]; then
     echo "Setting --overrides"
-    set -- "$@" --overrides "$ECS_PARAM_OVERRIDES"
+    set -- "$@" --overrides \""${ECS_PARAM_OVERRIDES}"\"
 fi
 if [ -n "$ECS_PARAM_TAGS" ]; then
     echo "Setting --tags"
