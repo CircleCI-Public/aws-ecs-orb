01981c80d39e4367e0e3b32029ab60ca54112735
Brian Vu <emailbrian.vu@gmail.com>
2022-07-13 01:00:18 -0700

    fix: added validation for edge cases

--- src/scripts/update-bluegreen-service-via-task-def.sh
+++ src/scripts/update-bluegreen-service-via-task-def.sh
@@ -12,12 +12,18 @@ if [ "$ECS_PARAM_ENABLE_CIRCUIT_BREAKER" == "1" ] && [ "$ECS_PARAM_VERIFY_REV_DE
     exit 1
 fi
 
-if [ -n "$ECS_PARAM_CAPACITY_PROVIDER_NAME" ] || [ -n "$ECS_PARAM_CAPACITY_PROVIDER_WEIGHT" ] || [ -n "$ECS_PARAM_CAPACITY_PROVIDER_BASE" ]; then 
-    echo "Capacity Provider name, base and weight parameter must all be provided. Please try again"
-    exit 1
+if [ -n "$ECS_PARAM_CD_CAPACITY_PROVIDER_NAME" ]; then 
+    if [ -z "$ECS_PARAM_CD_CAPACITY_PROVIDER_WEIGHT" ] || [ -z "$ECS_PARAM_CD_CAPACITY_PROVIDER_BASE" ]; then 
+        echo "Capacity Provider base and weight parameter must all be provided. Please try again"
+        exit 1
+    else 
+        REVISION="{\"revisionType\": \"AppSpecContent\", \"appSpecContent\": {\"content\": \"{\\\"version\\\": 1, \\\"Resources\\\": [{\\\"TargetService\\\": {\\\"Type\\\": \\\"AWS::ECS::Service\\\", \\\"Properties\\\": {\\\"TaskDefinition\\\": \\\"${CCI_ORB_AWS_ECS_REGISTERED_TASK_DFN}\\\", \\\"LoadBalancerInfo\\\": {\\\"ContainerName\\\": \\\"$ECS_PARAM_CD_LOAD_BALANCED_CONTAINER_NAME\\\", \\\"ContainerPort\\\": $ECS_PARAM_CD_LOAD_BALANCED_CONTAINER_PORT},\\\"CapacityProviderStrategy\\\":[{\\\"CapacityProvider\\\":\\\"$ECS_PARAM_CD_CAPACITY_PROVIDER_NAME\\\", \\\"Base\\\":${ECS_PARAM_CD_CAPACITY_PROVIDER_BASE}, \\\"Weight\\\":${ECS_PARAM_CD_CAPACITY_PROVIDER_WEIGHT}}]}}}]}\"}}"
+    fi
+else
+    REVISION="{\"revisionType\": \"AppSpecContent\", \"appSpecContent\": {\"content\": \"{\\\"version\\\": 1, \\\"Resources\\\": [{\\\"TargetService\\\": {\\\"Type\\\": \\\"AWS::ECS::Service\\\", \\\"Properties\\\": {\\\"TaskDefinition\\\": \\\"${CCI_ORB_AWS_ECS_REGISTERED_TASK_DFN}\\\", \\\"LoadBalancerInfo\\\": {\\\"ContainerName\\\": \\\"$ECS_PARAM_CD_LOAD_BALANCED_CONTAINER_NAME\\\", \\\"ContainerPort\\\": $ECS_PARAM_CD_LOAD_BALANCED_CONTAINER_PORT}}}}]}\"}}"
 fi 
 
-REVISION="{\"revisionType\": \"AppSpecContent\", \"appSpecContent\": {\"content\": \"{\\\"version\\\": 1, \\\"Resources\\\": [{\\\"TargetService\\\": {\\\"Type\\\": \\\"AWS::ECS::Service\\\", \\\"Properties\\\": {\\\"TaskDefinition\\\": \\\"${CCI_ORB_AWS_ECS_REGISTERED_TASK_DFN}\\\", \\\"LoadBalancerInfo\\\": {\\\"ContainerName\\\": \\\"$ECS_PARAM_CD_LOAD_BALANCED_CONTAINER_NAME\\\", \\\"ContainerPort\\\": $ECS_PARAM_CD_LOAD_BALANCED_CONTAINER_PORT}}}}]}\"}}"
+# REVISION="{\"revisionType\": \"AppSpecContent\", \"appSpecContent\": {\"content\": \"{\\\"version\\\": 1, \\\"Resources\\\": [{\\\"TargetService\\\": {\\\"Type\\\": \\\"AWS::ECS::Service\\\", \\\"Properties\\\": {\\\"TaskDefinition\\\": \\\"${CCI_ORB_AWS_ECS_REGISTERED_TASK_DFN}\\\", \\\"LoadBalancerInfo\\\": {\\\"ContainerName\\\": \\\"$ECS_PARAM_CD_LOAD_BALANCED_CONTAINER_NAME\\\", \\\"ContainerPort\\\": $ECS_PARAM_CD_LOAD_BALANCED_CONTAINER_PORT}}}}]}\"}}"
 #shellcheck disable=SC2086
 # APPSPEC=$(echo '{"version":1,"Resources":[{"TargetService":{"Type":"AWS::ECS::Service","Properties":{"TaskDefinition":"'${CCI_ORB_AWS_ECS_REGISTERED_TASK_DFN}'","LoadBalancerInfo":{"ContainerName":"'${ECS_PARAM_CD_LOAD_BALANCED_CONTAINER_NAME}'","ContainerPort":"'${ECS_PARAM_CD_LOAD_BALANCED_CONTAINER_PORT}'"},"CapacityProviderStrategy":[{"CapacityProvider":"FARGATE", "Base":0, "Weight":1}]}}}}]}' | jq -Rs .)
 # REVISION='{"revisionType":"AppSpecContent","appSpecContent":{"content":'${APPSPEC}'}}'
@@ -31,8 +37,7 @@ DEPLOYMENT_ID=$(aws deploy create-deployment \
     --deployment-group-name "$ECS_PARAM_CD_DEPLOY_GROUP_NAME" \
     --query deploymentId \
     --revision "${REVISION}" \
-    --output text \
-    --debug)
+    --output text)
 echo "Created CodeDeploy deployment: $DEPLOYMENT_ID"
 
 if [ "$ECS_PARAM_VERIFY_REV_DEPLOY" == "1" ]; then
