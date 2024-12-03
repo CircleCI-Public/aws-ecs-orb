2668e63ec4515c36952bcd42ec8825709c9984e3
Mateo Arboleda <mateo.arboleda@circleci.com>
2024-12-03 15:10:02 -0500

    Add functionality to create service if not created with ECS deployment

--- src/scripts/update_service_via_task_def.sh
+++ src/scripts/update_service_via_task_def.sh
@@ -8,11 +8,20 @@ ORB_STR_SERVICE_NAME="$(circleci env subst "$ORB_STR_SERVICE_NAME")"
 ORB_STR_PROFILE_NAME="$(circleci env subst "$ORB_STR_PROFILE_NAME")"
 ORB_AWS_REGION="$(circleci env subst "$ORB_AWS_REGION")"
 
+SERVICE_EXISTS=$(aws ecs describe-services \
+    --profile "${ORB_STR_PROFILE_NAME}" \
+    --cluster "$ORB_STR_CLUSTER_NAME" \
+    --services "${ORB_STR_SERVICE_NAME}" \
+    --query "services[?serviceName=='$ORB_STR_SERVICE_NAME'].serviceName" \
+    --region "${ORB_AWS_REGION}" \
+    --output text
+)
+echo "$SERVICE_EXISTS"
 if [ -z "${ORB_STR_SERVICE_NAME}" ]; then
     ORB_STR_SERVICE_NAME="$ORB_STR_FAMILY"
 fi
 
-if [ "$ORB_BOOL_FORCE_NEW_DEPLOY" == "1" ]; then
+if [ "$ORB_BOOL_FORCE_NEW_DEPLOY" == "1" ] && [ -n "$SERVICE_EXISTS" ]; then
     set -- "$@" --force-new-deployment
 fi
 
@@ -20,13 +29,33 @@ if [ "$ORB_BOOL_ENABLE_CIRCUIT_BREAKER" == "1" ]; then
     set -- "$@" --deployment-configuration "deploymentCircuitBreaker={enable=true,rollback=true}"
 fi
 
-DEPLOYED_REVISION=$(aws ecs update-service \
-    --profile "${ORB_STR_PROFILE_NAME}" \
-    --cluster "$ORB_STR_CLUSTER_NAME" \
-    --service "${ORB_STR_SERVICE_NAME}" \
-    --task-definition "${CCI_ORB_AWS_ECS_REGISTERED_TASK_DFN}" \
-    --output text \
-    --region "${ORB_AWS_REGION}" \
-    --query service.taskDefinition \
-    "$@")
-echo "export CCI_ORB_AWS_ECS_DEPLOYED_REVISION='${DEPLOYED_REVISION}'" >> "$BASH_ENV"
+if [ -n "$ORB_AWS_DESIRED_COUNT" ]; then
+    set -- "$@" --desired-count "$ORB_AWS_DESIRED_COUNT"
+fi
+
+if [ -z "$SERVICE_EXISTS" ]; then
+    echo "The service doesn't exist"
+    if [ "$ORB_AWS_CREATE_SERVICE" = 1 ]; then
+        NEW_SERVICE=$(aws ecs create-service \
+            --cluster "$ORB_STR_CLUSTER_NAME" \
+            --region "${ORB_AWS_REGION}" \
+            --profile "${ORB_STR_PROFILE_NAME}" \
+            --service-name "${ORB_STR_SERVICE_NAME}" \
+            --task-definition "${CCI_ORB_AWS_ECS_REGISTERED_TASK_DFN}" \
+            --network-configuration "awsvpcConfiguration={subnets=[$ORB_STR_SUBNETS],securityGroups=[$ORB_STR_SECURITY_GROUPS],assignPublicIp=$ORB_PUBLIC_IP}" \
+            --load-balancers "targetGroupArn=$ORB_STR_TARGET_GROUP,containerName=$ORB_STR_CONTAINER_NAME,containerPort=$ORB_CONTAINER_PORT" \
+            "$@")
+        echo "$NEW_SERVICE"
+    fi
+else
+    DEPLOYED_REVISION=$(aws ecs update-service \
+        --profile "${ORB_STR_PROFILE_NAME}" \
+        --cluster "$ORB_STR_CLUSTER_NAME" \
+        --service "${ORB_STR_SERVICE_NAME}" \
+        --task-definition "${CCI_ORB_AWS_ECS_REGISTERED_TASK_DFN}" \
+        --output text \
+        --region "${ORB_AWS_REGION}" \
+        --query service.taskDefinition \
+        "$@")
+    echo "export CCI_ORB_AWS_ECS_DEPLOYED_REVISION='${DEPLOYED_REVISION}'" >> "$BASH_ENV"
+fi
