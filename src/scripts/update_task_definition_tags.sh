36e3af990f119296ee02aafb8512703f327fa363
Maxine Aubrey <max@ine.dev>
2025-10-13 17:59:16 +0200

    feat: add tag change support to `update_task_definition`

---
+++ src/scripts/update_task_definition_tags.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o noglob
+
+ORB_STR_TASK_DEFINITION_TAGS="$(circleci env subst "$ORB_STR_TASK_DEFINITION_TAGS")"
+ORB_STR_PROFILE_NAME="$(circleci env subst "$ORB_STR_PROFILE_NAME")"
+ORB_AWS_REGION="$(circleci env subst "$ORB_AWS_REGION")"
+
+aws ecs tag-resource \
+	--resource-arn "${CCI_ORB_AWS_ECS_REGISTERED_TASK_DFN}" \
+	\ # shellcheck disable=SC2086
+	--tags ${ORB_STR_TASK_DEFINITION_TAGS} \
+	--profile "${ORB_STR_PROFILE_NAME}" \
+	--region "${ORB_AWS_REGION}"
